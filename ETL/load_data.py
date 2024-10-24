72677ebec902aa396f215a904edf84a42fc6c92e
Negroni1 <negronialanis2@gmail.com>
2024-10-24 18:54:39 -0400

    Add docker credentials

--- ETL/load_data.py
+++ ETL/load_data.py
@@ -264,11 +264,11 @@ def load_data():
 
     # Load data
     engine = psycopg2.connect(
-        dbname="dc79t7ga9hc6ud",
-        user="ufm5iffjti843g",
-        password="p714ce504f5566ea5085651f4627a98521b24b94298c88546efee8a7e038ad933",
-        host="cbdhrtd93854d5.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
-        port="5432"
+        dbname="excel_db",
+        user="excel",
+        password="password",
+        host="localhost",
+        port="1234"
     )
     cursor = engine.cursor()
     create_db(cursor)
