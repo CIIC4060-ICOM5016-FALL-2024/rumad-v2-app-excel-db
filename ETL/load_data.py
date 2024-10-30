94cf9b1ad749db0bd1502495d7cd82576ae6255e
Negroni1 <negronialanis2@gmail.com>
2024-10-29 20:18:12 -0400

    Add heroku credentials

--- ETL/load_data.py
+++ ETL/load_data.py
@@ -262,13 +262,22 @@ def load_data():
     # Transform data
     sections, meetings, rooms, courses = transform_data(sections, meetings, rooms, courses)
 
-    # Load data
+    # Test Load data
+    # engine = psycopg2.connect(
+    #     dbname="excel_db",
+    #     user="excel",
+    #     password="password",
+    #     host="localhost",
+    #     port="1234"
+    # )
+
+    # Heroku Load data
     engine = psycopg2.connect(
-        dbname="excel_db",
-        user="excel",
-        password="password",
-        host="localhost",
-        port="1234"
+        dbname="dc79t7ga9hc6ud",
+        user="ufm5iffjti843g",
+        password="p714ce504f5566ea5085651f4627a98521b24b94298c88546efee8a7e038ad933",
+        host="cbdhrtd93854d5.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com",
+        port="5432"
     )
     cursor = engine.cursor()
     create_db(cursor)
