7df4fba344e0f3ccfd9fb11d1b460e09157a8f68
Negroni1 <negronialanis2@gmail.com>
2024-11-10 22:36:15 -0400

    Fix credentials

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -11,7 +11,7 @@
 pg_config = {
     'user' : 'ufm5iffjti843g',
     'password' : 'p714ce504f5566ea5085651f4627a98521b24b94298c88546efee8a7e038ad933',
-    'database' : 'dc79t7ga9hc6ud',
+    'dbname' : 'dc79t7ga9hc6ud',
     'host' : 'cbdhrtd93854d5.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
     'port': '5432',
 }
