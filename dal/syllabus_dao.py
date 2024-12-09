5ac27b0b1a029708606305debb3ce820535cdbb7
Negroni1 <negronialanis2@gmail.com>
2024-12-09 13:53:20 -0400

    Fix embedding distance query

--- dal/syllabus_dao.py
+++ dal/syllabus_dao.py
@@ -48,7 +48,7 @@ class SyllabusDAO(DAO):
         query = """SELECT chunkid, courseid, embedding_text <-> %s as distance, chunk 
                     FROM syllabus 
                     ORDER BY distance 
-                    LIMIT 3"""
+                    LIMIT 5"""
         values = [emb, ]
         return self.read(query, values)
 
